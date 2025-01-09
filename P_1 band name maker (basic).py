fafadfb7c7c8e2ed27ac7847aea0b4f765727c7a
Dhruv_Dahiya <110717843+CodeWithDhruvv@users.noreply.github.com>
2025-01-09 23:16:15 +0000

    Update P_1 band name maker (basic).py

--- P_1 band name maker (basic).py
+++ P_1 band name maker (basic).py
@@ -2,4 +2,4 @@ print ("welcome to band name generator")
 n = input("what is name of your city?  \n")
 p = input("what is name of your pet?   \n")
 
-print (f"your band name could be", n ,p )
+print (f"your band name could be", n ,p)
