b46152a079168110361a701c6f34644baa4039da
Dhruv_Dahiya <110717843+CodeWithDhruvv@users.noreply.github.com>
2025-06-20 15:07:08 +0100

    Add files via upload
    This is code forday 16, today we have  learned what is oop and how we can modify our previous coffee machine code using object oriented programming.

--- main.py
+++ main.py
@@ -1,66 +1,23 @@
-import random
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from replit import clear
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-from art import logo, vs
-from game_data import data
+is_on = True
 
-
-def get_random_account():
-  """Get data from random account"""
-  return random.choice(data)
-
-def format_data(account):
-  """Format account into printable format: name, description and country"""
-  name = account["name"]
-  description = account["description"]
-  country = account["country"]
-  # print(f'{name}: {account["follower_count"]}')
-  return f"{name}, a {description}, from {country}"
-
-def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
-  if a_followers > b_followers:
-    return guess == "a"
-  else:
-    return guess == "b"
-
-
-def game():
-  print(logo)
-  score = 0
-  game_should_continue = True
-  account_a = get_random_account()
-  account_b = get_random_account()
-
-  while game_should_continue:
-    account_a = account_b
-    account_b = get_random_account()
-
-    while account_a == account_b:
-      account_b = get_random_account()
-
-    print(f"Compare A: {format_data(account_a)}.")
-    print(vs)
-    print(f"Against B: {format_data(account_b)}.")
-    
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    a_follower_count = account_a["follower_count"]
-    b_follower_count = account_b["follower_count"]
-    is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-    clear()
-    print(logo)
-    if is_correct:
-      score += 1
-      print(f"You're right! Current score: {score}.")
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
-      game_should_continue = False
-      print(f"Sorry, that's wrong. Final score: {score}")
-
-game()
-
-
-
+        drink = menu.find_drink(choice)
+        
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
